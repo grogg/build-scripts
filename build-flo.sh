68a49df21de6f2fb33e244c31b7452cf41c48d69
grogg <lee.joshwa@gmail.com>
2015-04-17 11:11:58 -0700

    ;Revert "Revert "remove kernel make from scripts""
    This reverts commit df46dd5bd1413d82e44bde71bdcaea90226138c7.
    
    Conflicts:
    	build-all-nok.sh
    	build-all.sh

--- build-flo.sh
+++ build-flo.sh
@@ -1,12 +1,3 @@
-export PLATFORM_DIR=~/AOSP/device/asus/flo-kernel
-export ARCH=arm
-export CROSS_COMPILE=arm-eabi-
-export PATH=~/AOSP/prebuilts/gcc/linux-x86/arm/arm-eabi-4.8/bin:$PATH
-cd $PLATFORM_DIR/kernel
-make mrproper
-make flo_defconfig
-make -j4
-cd ~/AOSP
 . build/envsetup.sh
 make clobber
 lunch aosp_flo-userdebug
